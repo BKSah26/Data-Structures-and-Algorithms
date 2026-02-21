92e9e28a9440d75640b4d252f3166edb9c9aab30
Bipin Kumar Sah <sahbipinkr2006@gmail.com>
2026-02-21 12:20:26 +0530

    GCD

---
+++ src/Recursion/GCD.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class GCD {
+    public static int gcd(int a, int b){
+        if (b == 0){
+            return a;
+        }
+        return gcd(b, a%b);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter A: ");
+        int a = sc.nextInt();
+        System.out.print("Enter B: ");
+        int b = sc.nextInt();
+
+        int hcf = gcd(a, b);
+        System.out.println(hcf);
+    }
+}
