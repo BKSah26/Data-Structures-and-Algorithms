3ee8cda33758c4b9e34fbe9c20d954dc31050db3
Bipin Kumar Sah <sahbipinkr2006@gmail.com>
2026-09-09 09:15:18 +0530

    Add files via upload

---
+++ src/Dynamic_Programming/ZeroOneKnapsack.java
@@ -0,0 +1,48 @@
+package Dynamic_Programming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ZeroOneKnapsack {
+    public static int Knapsack(int i, int[] wt, int[] pf, int C, int[][] dp){
+        if (i==wt.length){
+            return 0;
+        }
+        if (dp[i][C]!=-1){
+            return dp[i][C];
+        }
+        int skip = Knapsack(i+1, wt, pf, C, dp);
+        if (wt[i]>C){
+            dp[i][C] = skip;
+            return skip;
+        }
+        int take = pf[i]+Knapsack(i+1, wt, pf, C-wt[i], dp);
+        dp[i][C] =Math.max(take, skip);
+        return Math.max(take, skip);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no. of items: ");
+        int n = sc.nextInt();
+        int[] wt = new int[n];
+        int[] pf = new int[n];
+
+        System.out.println("Enter the weight and profit of the items: ");
+        for (int i=0; i<n; i++){
+            wt[i] = sc.nextInt();
+            pf[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter the capacity of the Knapsack: ");
+        int C = sc.nextInt();
+
+        int[][] dp = new int[n][C+1];
+        for (int i=0; i<n; i++){
+            Arrays.fill(dp[i], -1);
+        }
+
+        int maxProfit = Knapsack(0, wt, pf, C, dp);
+        System.out.println("Maximum Profit: "+maxProfit);
+    }
+}
